1942e51a3b2c63a55b6f0269c239f5fb436e63d9
swathi rao <swathiraomce@gmail.com>
2019-06-01 08:57:41 +0530

    service for product sub category

--- app/src/main/java/com/ayushya/spring/bean/Models.java
+++ app/src/main/java/com/ayushya/spring/bean/Models.java
@@ -2,64 +2,88 @@ package com.ayushya.spring.bean;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @SuppressWarnings("serial")
 @Document(collection = "models")
-public class Models {
+public class Models
+{
 
-    @Id ObjectId databaseId;
-    @Field("id")
-    private String id;
-    @Field("model")
-    private String model;
-    @Field("product_id")
-    private int product_id;
-    @Field("model_id")
-    private int model_id;
-    @Field("brand_id")
-    private int brand_id;
-    
-	public Models(String id, String model, int product_id, int model_id, int brand_id) {
-		this.id = id;
-		this.model = model;
-		this.product_id = product_id;
-		this.model_id = model_id;
-		this.brand_id = brand_id;
-	}
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getModel() {
-		return model;
-	}
-	public void setModel(String model) {
-		this.model = model;
-	}
-	public int getProduct_id() {
-		return product_id;
-	}
-	public void setProduct_id(int product_id) {
-		this.product_id = product_id;
-	}
-	public int getModel_id() {
-		return model_id;
-	}
-	public void setModel_id(int model_id) {
-		this.model_id = model_id;
-	}
-	public int getBrand_id() {
-		return brand_id;
-	}
-	public void setBrand_id(int brand_id) {
-		this.brand_id = brand_id;
-	}
-    
-	
+  @Id
+  ObjectId databaseId;
+  @Field("id")
+  private String id;
+  @Field("model")
+  private String model;
+  @Field("product_id")
+  private int product_id;
+  @Field("brand_id")
+  private int brand_id;
+  @Field("productSubCat_id")
+  private int productSubCat_id;
+
+  public Models(String id,
+                String model,
+                int product_id,
+                int brand_id,
+                int productSubCat_id)
+  {
+    this.id = id;
+    this.model = model;
+    this.product_id = product_id;
+    this.brand_id = brand_id;
+    this.productSubCat_id = productSubCat_id;
+  }
+
+  public String getId()
+  {
+    return id;
+  }
+
+  public void setId(String id)
+  {
+    this.id = id;
+  }
+
+  public String getModel()
+  {
+    return model;
+  }
+
+  public void setModel(String model)
+  {
+    this.model = model;
+  }
+
+  public int getProduct_id()
+  {
+    return product_id;
+  }
+
+  public void setProduct_id(int product_id)
+  {
+    this.product_id = product_id;
+  }
+
+  public int getBrand_id()
+  {
+    return brand_id;
+  }
+
+  public void setBrand_id(int brand_id)
+  {
+    this.brand_id = brand_id;
+  }
+
+  public int getProductSubCat_id()
+  {
+    return productSubCat_id;
+  }
+
+  public void setProductSubCat_id(int productSubCat_id)
+  {
+    this.productSubCat_id = productSubCat_id;
+  }
+
 }
--- app/src/main/java/com/ayushya/spring/service/UserService.java
+++ app/src/main/java/com/ayushya/spring/service/UserService.java
@@ -9,32 +9,58 @@ import com.ayushya.spring.bean.Cities;
 import com.ayushya.spring.bean.Employee;
 import com.ayushya.spring.bean.Models;
 import com.ayushya.spring.bean.Product_category;
+import com.ayushya.spring.bean.Product_subCategory;
 import com.ayushya.spring.bean.States;
 
-public interface UserService {
-	public void createUser(Employee user);
-	public List<Employee> getUser();
-	public Employee findById(String id);
-	public Employee update(Employee user, String l);
-	public void deleteUserById(String id);
-	public Employee updatePartially(Employee user, String id);
+public interface UserService
+{
+  public void createUser(Employee user);
 
-	public String uploadImage(MultipartFile uploadfile);
+  public List<Employee> getUser();
 
-	public List<Cities> getCityByState(int id);
-	public List<Cities> getCities();
-	public Cities getCityById(String id);
+  public Employee findById(String id);
 
-	public List<States> getStates();
+  public Employee update(Employee user,
+                         String l);
 
-	public List<Employee> findByName(String name);
-	public List<Employee> findByNameStartingWith(String regexp);
-	public List<Employee> findByNameEndingWith(String regexp);
-	public List<Employee> findByAgeBetween(int ageGT, int ageLT);
-	
-	public List<Brands> getBrands();
-	public List<Product_category> getProducts();
-	public List<Product_category> getProducts(int brand_id);
-	public List<Models> getModels();
-	public List<Models> getModels(int brand_id,int product_id);
+  public void deleteUserById(String id);
+
+  public Employee updatePartially(Employee user,
+                                  String id);
+
+  public String uploadImage(MultipartFile uploadfile);
+
+  public List<Cities> getCityByState(int id);
+
+  public List<Cities> getCities();
+
+  public Cities getCityById(String id);
+
+  public List<States> getStates();
+
+  public List<Employee> findByName(String name);
+
+  public List<Employee> findByNameStartingWith(String regexp);
+
+  public List<Employee> findByNameEndingWith(String regexp);
+
+  public List<Employee> findByAgeBetween(int ageGT,
+                                         int ageLT);
+
+  public List<Brands> getBrands();
+
+  public List<Product_category> getProducts();
+
+  public List<Product_subCategory> getProductSubCategory();
+
+  public List<Product_category> getProducts(int brand_id);
+
+  public List<Models> getModels();
+
+  public List<Models> getModels(int brand_id,
+                                int product_id,
+                                int productSubCat_id);
+
+  List<Product_subCategory> getProductSubCategory(int brand_id,
+                                                  int product_id);
 }
